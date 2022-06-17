dd6b99b0ae8bc98cab4167daae21f2e8926cf179
Ashforestunknown <Ashforestunknown@gmail.com>
2022-06-17 18:43:17 +0300

    Ahmed is done, Mohammed is here to save the day, please

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -10,12 +10,15 @@ using namespace std;
 
 //Function prototyping
 
-void Add_ID_To_Database(string ID);
-bool Check_If_Present(string ID);
-void Edit_Student_Data(string ID, int i);
-void Delete_Student_Record(string ID);
-void AddStudentData();
-int StudentCounter();
+void Add_ID_To_Database(string ID); //Adds a new ID to a database of IDs
+bool Check_If_Present(string ID); //Returns a boolean depending on the presence of the ID entered in the parameter
+void Edit_Student_Data(string ID, int i); //Edits student data based on the integer entered in (handled in the front end)
+void Delete_Student_Record(string ID); //Removes an entire text file of the student as well as all reference to that student
+void AddStudentData(); //Makes a text file for the student and add the ID to the ID database
+int StudentCounter(); //Counts the amount of students in the ID database
+int PasswordsCounter(); //Counts the amount of student with Passwords (pretty useless but there if needed)
+void MakePassword(); //Makes a password for the student
+bool Check_Password(string ID, string password); //Checks the validity of the password
 
 //Classes
 
@@ -23,7 +26,6 @@ struct Student
 {
 	enum class StudentInputs
 	{
-		//ID,
 		FName,
 		LName,
 		Age,
@@ -47,7 +49,17 @@ class Teacher
 
 int main()
 {
-	AddStudentData();
+	/*
+		All that is needed is the front end.
+		The Edit function has an integer to pass into it which decides the thing we want to edit.
+		So when you want to use it have it be decided with an integer that goes up and down with the arrows.
+		Oh yeah, the way the user will choose which to edit is by a pointer (not c++ pointer).
+		This pointer will point towards the thing we want to edit and an integer will be changing depending on the pointer.
+		Don't mind the password functions, they were annoying to make in the first place.
+		And please please please make the front end, you don't have to make the program all over again or in your way.
+		If you have any problem just ask me.
+		I am tired
+	*/
 }
 
 //Additional functions other than main
@@ -84,23 +96,7 @@ void Edit_Student_Data(string ID, int i)
 	//Loads all of the data to the RAM
 
 	switch (Student::StudentInputs(i)) //jumps to the wanted data to change
-	{/*
-	case Student::StudentInputs::ID:
 	{
-		cout << "WARNING!!! changing the ID will change the name of the file\n";
-		char check;
-		cout << "Do you still want to continue and change the ID? press y, otherwise press anything else\n";
-		cin >> check;
-		if (check != 'y')
-		{
-			break;
-		}
-		string ID;
-		cout << "Enter the new ID\n";
-		cin >> ID;
-		s.ID = ID;
-	}*/
-	break;
 	case Student::StudentInputs::FName:
 	{
 		string FName;
@@ -146,18 +142,49 @@ void Edit_Student_Data(string ID, int i)
 			<< s.University << " "
 			<< s.Level;
 	//Takes all the data from the RAM and stores in into the text file with the edited data
-
-	//string oldname = "C:/Users/Ashfo/OneDrive/Desktop/Big project/Test/Test/Database/" + ID + ".txt";
-	//string newname = "C:/Users/Ashfo/OneDrive/Desktop/Big project/Test/Test/Database/" + s.ID + ".txt";
-	////Stores the names to be renamed
-	//rename(oldname.c_str(), newname.c_str());
-	////Renames the text file to fit the ID of the student if changed otherwise it will rename to the same name
 }
 
 void Delete_Student_Record(string ID)
 {
 	string filename = "Database/" + ID + ".txt";
 	remove(filename.c_str());
+	{
+		ifstream file("Database/Student ID List.txt");
+		vector<string> FileData;
+		FileData.resize(StudentCounter());
+		for (string& FD : FileData)
+		{
+			file >> FD;
+		}
+		ofstream IDs("Database/Student ID List.txt");
+		IDs.clear();
+		for (unsigned int i = 0; i < FileData.size(); ++i)
+		{
+			if (FileData.at(i) != ID)
+			{
+				IDs << FileData.at(i) << "\n";
+			}
+		}
+	}
+	{
+		ifstream file("Database/Student Passwords.txt");
+		vector<string> FileData;
+		FileData.resize(PasswordsCounter() * 2);
+		for (string& FD : FileData)
+		{
+			file >> FD;
+		}
+		ofstream IDs("Database/Student Passwords.txt");
+		IDs.clear();
+		IDs << "\n";
+		for (unsigned int i = 0; i < FileData.size(); i += 2)
+		{
+			if (FileData.at(i) != ID)
+			{
+				IDs << FileData.at(i) << " " << FileData.at(i + 1) << "\n";
+			}
+		}
+	}
 }
 
 void AddStudentData()
@@ -209,4 +236,72 @@ int StudentCounter()
 		}
 	}
 	return counter;
+}
+
+int PasswordsCounter()
+{
+	ifstream Passwords("Database/Student Passwords.txt");
+	int counter = 0;
+	while (Passwords)
+	{
+		char c = Passwords.get();
+		if (c == 10)
+		{
+			++counter;
+		}
+	}
+	return (counter - 1);
+}
+
+void MakePassword()
+{
+	label:
+	cout << "Enter your ID: ";
+	string ID;
+	cin >> ID;
+	string password;
+	if (Check_If_Present(ID))
+	{
+		cout << "Enter the password: ";
+		cin >> password;
+		cout << "Don't forget this password, you cannot get it back and there is no way to retrieve your credentials so memorize\n";
+		ofstream Passes("Database/Student Passwords.txt", ios::app);
+		Passes << ID << " " << password << "/n";
+	}
+	else
+	{
+		cout << "The student does not exist, try again";
+		goto label;
+	}
+}
+
+bool Check_Password(string ID, string password)
+{
+	ifstream Pass("Database/Student Passwords.txt");
+	if (Check_If_Present(ID))
+	{
+		while (Pass)
+		{
+			char c = Pass.get();
+			if (c == 10)
+			{
+				string IDs;
+				Pass >> IDs;
+				if (ID == IDs)
+				{
+					string codeword;
+					Pass >> codeword;
+					if (password == codeword)
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+		}
+	}
+	return false;
 }
